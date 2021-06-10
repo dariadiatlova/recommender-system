50b52b1919f22824c0c99346389b2390d4b9ae6c
diatlova <dari.diatlova@gmail.com>
2021-06-11 00:00:04 +0300

    Adding datapreprocessing block

---
+++ src/common/util.py
@@ -0,0 +1,42 @@
+import random
+from enum import Enum, unique
+import pandas as pd
+import numpy as np
+from data import MAIN_FOLDER
+
+
+@unique
+class DatasetColumnName(Enum):
+    USER_ID = 'userId'
+    MOVIE_ID = 'movieId'
+    TIMESTAMP = 'timestamp'
+
+
+@unique
+class TrainTestSize(Enum):
+    MIN_TEST_SIZE = 0.25
+    MAX_TEST_SIZE = 0.35
+    TARGET_TEST_SIZE = 0.3
+    SAMPLE_SIZE = 5000
+
+
+def get_user_timestamp_threshold():
+    """ 1. Picks a sample_size of unique ids of users.
+        2. Sorts timestamps of chosen users in ascending order.
+        3. Takes the the percentile timestamp.
+        4. Returns the median of computed timestamp percentiles.
+    """
+    df = pd.read_csv(MAIN_FOLDER.parent / 'rating.csv')
+    user_id = np.unique(df[DatasetColumnName.USER_ID.value].to_numpy())
+    user_id_sample = random.sample(list(user_id), TrainTestSize.SAMPLE_SIZE.value)
+    thirty_percentile_timestamp = []
+
+    for user in user_id_sample:
+        user_timestamp = df.loc[df[DatasetColumnName.USER_ID.value] == user, DatasetColumnName.TIMESTAMP.value]
+        sorted_timestamp = user_timestamp.sort_values(ascending=False).to_numpy()
+        current_percentile = sorted_timestamp[int(len(sorted_timestamp) * TrainTestSize.TARGET_TEST_SIZE.value)]
+        thirty_percentile_timestamp.append(current_percentile)
+
+    sorted_percentile_timestamp = pd.Series(thirty_percentile_timestamp).sort_values(ascending=False)
+    optimal_split_value = int(TrainTestSize.SAMPLE_SIZE.value * 0.6)
+    return sorted_percentile_timestamp.to_numpy()[optimal_split_value]
---
+++ src/preprocessing/split_dataset.py
@@ -0,0 +1,37 @@
+import pandas as pd
+import numpy as np
+import logging.config
+
+
+from data import MAIN_FOLDER
+from src.common.util import DatasetColumnName, TrainTestSize, get_user_timestamp_threshold
+
+logger = logging.getLogger(__name__)
+
+
+def __get_time_wise_split():
+    for _ in range(5):
+        threshold_timestamp = get_user_timestamp_threshold()
+        df = pd.read_csv(MAIN_FOLDER.parent / 'rating.csv')
+        train_df = df[df[DatasetColumnName.TIMESTAMP.value] > threshold_timestamp]
+        test_df = df[df[DatasetColumnName.TIMESTAMP.value] <= threshold_timestamp]
+        test_size = np.round(test_df.shape[0] / df.shape[0], 2)
+        print(f'Found a split with test size: ~{test_size}')
+        if TrainTestSize.MIN_TEST_SIZE.value < test_size < TrainTestSize.MAX_TEST_SIZE.value:
+            print(f'Seems good enough, will save!')
+            return train_df, test_df
+
+
+def main():
+    try:
+        train_df, test_df = __get_time_wise_split()
+        train_data, test_data = train_df.iloc[:, :3], test_df.iloc[:, :3]
+        train_data.to_csv(MAIN_FOLDER.parent / 'train_rating.csv', index=False)
+        test_data.to_csv(MAIN_FOLDER.parent / 'test_rating.csv', index=False)
+    except TypeError as e:
+        logger.error('Could not find the split to satisfy chosen parameters :( Try again or change the parameters!')
+        raise e
+
+
+if __name__ == '__main__':
+    main()
