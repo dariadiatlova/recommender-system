70561c854d535be2f569a898910ccaaf9c554d32
diatlova <dari.diatlova@gmail.com>
2021-06-16 15:49:42 +0300

    added functionality to filter tag.csv

--- src/preprocessing/filter_data.py
+++ src/preprocessing/filter_data.py
@@ -6,16 +6,24 @@ from src.common.util import DatasetColumnName
 
 def filter_data():
     """ Remove movies from the dataset that were rated by less than 1% of users """
-    df = pd.read_csv(MAIN_FOLDER.parent / 'rating.csv')
-    n_users = int(len(df[DatasetColumnName.USER_ID.value].unique()) * 0.01)
-    print(f'Initial dataset size: {df.shape[0]} ratings')
-    grouped = df.groupby([DatasetColumnName.MOVIE_ID.value])
+    df_rating = pd.read_csv(MAIN_FOLDER.parent / 'rating.csv')
+    df_tag = pd.read_csv(MAIN_FOLDER.parent / 'tag.csv')
+
+    n_users = int(len(df_rating[DatasetColumnName.USER_ID.value].unique()) * 0.01)
+    print(f'Initial dataset size: {df_rating.shape[0]} ratings')
+
+    grouped = df_rating.groupby([DatasetColumnName.MOVIE_ID.value])
     n_movies = grouped.size()
     index_names = n_movies[n_movies > n_users].index
-    filtered_df = df[df[DatasetColumnName.MOVIE_ID.value].isin(index_names)]
-    print(f'Filtered dataset size: {filtered_df.shape[0]} ratings')
-    print(f'Reduced dataset size on {np.round((df.shape[0] - filtered_df.shape[0]) / df.shape[0], 2) * 100}%')
-    filtered_df.to_csv(MAIN_FOLDER.parent / 'filtered_rating.csv', index=False)
+
+    filtered_df_rating = df_rating[df_rating[DatasetColumnName.MOVIE_ID.value].isin(index_names)]
+    filtered_df_tag = df_tag[df_tag[DatasetColumnName.MOVIE_ID.value].isin(index_names)]
+
+    print(f'Filtered dataset size: {filtered_df_rating.shape[0]} ratings')
+    print(f'Reduced dataset size on {np.round((df_rating.shape[0] - filtered_df_rating.shape[0]) / df_rating.shape[0], 2) * 100}%')
+
+    filtered_df_rating.to_csv(MAIN_FOLDER.parent / 'filtered_rating.csv', index=False)
+    filtered_df_tag.to_csv(MAIN_FOLDER.parent / 'filtered_tag.csv', index=False)
 
 
 if __name__ == "__main__":
